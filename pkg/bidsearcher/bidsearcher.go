3a0dc448376d0820bf224d2a49c69cf75232da8e
dennischou <dses5900@gmail.com>
2021-03-14 11:02:12 +0800

    Complete the get bid function 1. extract methods 2. add next page url parsing 3. fix bugs when there's no date and award infos 4. fix bugs when there's extra new line when parsing

--- pkg/bidsearcher/bidsearcher.go
+++ pkg/bidsearcher/bidsearcher.go
@@ -15,7 +15,10 @@ import (
 )
 
 type BidSearcher struct {
-	client *http.Client
+	client     *http.Client
+	baseUrl    string
+	loginPath  string
+	searchPath string
 }
 
 func NewBidSearcher() *BidSearcher {
@@ -26,25 +29,66 @@ func NewBidSearcher() *BidSearcher {
 	client := &http.Client{
 		Jar: jar,
 	}
-	return &BidSearcher{client: client}
+	return &BidSearcher{
+		client:     client,
+		baseUrl:    "https://www.taiwanbuying.com.tw/",
+		loginPath:  "MemLoginAction.asp",
+		searchPath: "QueryCloseCaseAction.ASP",
+	}
 }
 
-func (b *BidSearcher) Search(keywork string) []Bid {
+func (b *BidSearcher) Search(keywork string) []*Bid {
 	err := b.login()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Login", err)
 	}
 
-	resp, err := b.client.PostForm("https://www.taiwanbuying.com.tw/QueryCloseCaseAction.ASP", url.Values{"DataType": {"OBJ"}, "Keyword": {"機油"}})
+	var bids []*Bid
+	doc, err := b.getDocumentByPost(b.baseUrl+b.searchPath, url.Values{"DataType": {"OBJ"}, "Keyword": {"機油"}})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(b.baseUrl+b.searchPath, err)
+	}
+	bids = append(bids, b.getBids(doc)...)
+
+	for {
+		nextPage, exists := b.getNextPage(doc)
+		if !exists {
+			break
+		}
+		doc, err = b.getDocumentByGet(nextPage)
+		if err != nil {
+			log.Fatal(nextPage, err)
+		}
+		bids = append(bids, b.getBids(doc)...)
+	}
+	return bids
+}
+
+func (b *BidSearcher) login() error {
+	resp, err := b.client.PostForm(b.baseUrl+b.loginPath, url.Values{"LogID": {"cindy886637"}, "PWD": {"Inipass07242020"}, "Submit": {"送出"}})
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New("Log in failed with " + strconv.Itoa(resp.StatusCode))
+	}
+	return nil
+}
+
+func (b *BidSearcher) getDocumentByPost(url string, data url.Values) (*goquery.Document, error) {
+	resp, err := b.client.PostForm(url, data)
+	if err != nil {
+		return nil, err
 	}
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	return doc, nil
+}
 
+func (b *BidSearcher) getBids(doc *goquery.Document) []*Bid {
 	var bids []*Bid
 	doc.Find("td[valign=top] > table").Eq(2).Find("td > table").Each(func(i int, s *goquery.Selection) {
 		s = s.Find("td").Eq(1)
@@ -55,37 +99,36 @@ func (b *BidSearcher) Search(keywork string) []Bid {
 			log.Fatal(err)
 		}
 		bids = append(bids, bid)
-
 	})
-	return nil
-}
-
-func (b *BidSearcher) login() error {
-	resp, err := b.client.PostForm("https://www.taiwanbuying.com.tw/MemLoginAction.asp", url.Values{"LogID": {"cindy886637"}, "PWD": {"Inipass07242020"}, "Submit": {"送出"}})
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return errors.New("Log in failed with " + strconv.Itoa(resp.StatusCode))
-	}
-	return nil
+	return bids
 }
 
 func (b *BidSearcher) parse(text string) (*Bid, error) {
 	lines := strings.Split(text, "\n")
-	unit := strings.Split(lines[0], ":")[1]
-	caseName := strings.Split(lines[1], ":")[1]
-	vendor := strings.Split(lines[2], ":")[1]
-
-	re := regexp.MustCompile(`[\d,/]+`)
-	matches := re.FindAllString(lines[3], -1)
-	award, err := strconv.Atoi(strings.ReplaceAll(matches[0], ",", ""))
-	if err != nil {
-		return nil, err
+	var cleanLines []string
+	for _, line := range lines {
+		if line != "" {
+			cleanLines = append(cleanLines, line)
+		}
 	}
-	date, err := time.Parse("2006/1/2", matches[1])
-	if err != nil {
-		return nil, err
+	unit := strings.Split(cleanLines[0], ":")[1]
+	caseName := strings.Split(cleanLines[1], ":")[1]
+	vendor := strings.Split(cleanLines[2], ":")[1]
+
+	var err error
+	var award int
+	var date time.Time
+	if len(cleanLines) == 4 {
+		re := regexp.MustCompile(`[\d,/]+`)
+		matches := re.FindAllString(cleanLines[3], -1)
+		award, err = strconv.Atoi(strings.ReplaceAll(matches[0], ",", ""))
+		if err != nil {
+			return nil, err
+		}
+		date, err = time.Parse("2006/1/2", matches[1])
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	bid := &Bid{
@@ -96,4 +139,22 @@ func (b *BidSearcher) parse(text string) (*Bid, error) {
 		date:     date,
 	}
 	return bid, nil
+}
+
+func (b *BidSearcher) getNextPage(doc *goquery.Document) (string, bool) {
+	nextPage, exists := doc.Find("#Pagers td[align=left] a").First().Attr("href")
+	return b.baseUrl + nextPage, exists
+}
+
+func (b *BidSearcher) getDocumentByGet(url string) (*goquery.Document, error) {
+	resp, err := b.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
 }
